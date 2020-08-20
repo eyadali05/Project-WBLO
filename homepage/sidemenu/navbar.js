1437064eb9073da3657b81b396e9616136b6dffe
Eyad Ali <razerpy@gmail.com>
2020-08-20 23:00:28 +0200

    Added part 1 of frontend files

---
+++ homepage/sidemenu/navbar.js
@@ -0,0 +1,26 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    //nav toggler
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        //link animation
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.25}s`;
+            }
+        });
+
+        //Burger animation
+        burger.classList.toggle('toggle');
+
+    });
+
+
+}
+
+navSlide();
